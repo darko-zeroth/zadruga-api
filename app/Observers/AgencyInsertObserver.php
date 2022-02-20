4bd0a81289d06225370805e8dbbecaae77d5562c
darko <djordjevic.darko@gmail.com>
2022-02-20 16:00:05 +0100

    Scope and observer for AgencyTrait

---
+++ app/Observers/AgencyInsertObserver.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Observers;
+
+use Illuminate\Support\Facades\Auth;
+
+class AgencyInsertObserver
+{
+	public function saving($model)
+	{
+		$model->agency_id = Auth::user()->agency_id;
+	}
+}
