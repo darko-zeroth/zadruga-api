e8defa20ca1483ba0bdc21f4d0e0c0301dda43eb
darko <djordjevic.darko@gmail.com>
2022-02-26 19:14:56 +0100

    Agency scope bug

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
+use App\Scopes\AgencyScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
     use HasFactory;
+
+	/**
+	 * The "booted" method of the model.
+	 *
+	 * @return void
+	 */
+	protected static function booted()
+	{
+		static::addGlobalScope(new AgencyScope());
+	}
 
 	protected $guarded = [
 		'id',
