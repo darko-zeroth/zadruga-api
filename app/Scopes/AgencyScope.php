32009073a8cc04123dc8001f1bc68e1dce2db959
darko <djordjevic.darko@gmail.com>
2022-02-27 16:50:38 +0100

    Agency Scope conditional

--- app/Scopes/AgencyScope.php
+++ app/Scopes/AgencyScope.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Schema;
 
 class AgencyScope implements Scope
 {
@@ -18,6 +19,8 @@ class AgencyScope implements Scope
 	 */
 	public function apply(Builder $builder, Model $model)
 	{
-		$builder->where('agency_id', '=', Auth::user()->agency_id);
+		if(Schema::hasColumn($builder->getModel()->getTable(), 'agency_id')){
+			$builder->where('agency_id', '=', Auth::user()->agency_id);
+		}
 	}
 }
