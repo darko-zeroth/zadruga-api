aa72baddccfa1ffb905be7923a20a222749c3aac
darko <djordjevic.darko@gmail.com>
2022-03-13 15:25:36 +0100

    Added unique attribute to the description field on create profile migraiton

--- database/migrations/2022_02_13_192902_create_profiles_table.php
+++ database/migrations/2022_02_13_192902_create_profiles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-			$table->string('description');
+			$table->string('description')->unique();
 			$table->foreignId('country_id')->constrained();
             $table->timestamps();
 
